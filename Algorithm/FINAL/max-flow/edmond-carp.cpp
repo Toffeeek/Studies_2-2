490ab60abc85cece4b74e0af94b85874a00fa4d3
Toffeeek <tawfiq.ahmed.1501@gmail.com>
2026-09-04 10:39:12 +0600

    sum changers

--- Algorithm/FINAL/max-flow/edmond-carp.cpp
+++ Algorithm/FINAL/max-flow/edmond-carp.cpp
@@ -7,7 +7,8 @@ const int INF = 1e9;
 int bfs(int source, int sink,
         vector<vector<int>>& capacity,
         vector<vector<int>>& adj,
-        vector<int>& parent) {
+        vector<int>& parent)
+{
 
     fill(parent.begin(), parent.end(), -1);
     parent[source] = -2;
@@ -39,77 +40,77 @@ int bfs(int source, int sink,
     }
 
     return 0;
+}
+
+// Edmonds-Karp algorithm
+int maxFlow(int source, int sink,
+            vector<vector<int>>& capacity,
+            vector<vector<int>>& adj,
+            int n) {
+
+    int flow = 0;
+    vector<int> parent(n);
+
+    int newFlow;
+
+    while ((newFlow = bfs(source, sink,
+        capacity, adj, parent))) {
+
+        flow += newFlow;
+
+    int current = sink;
+
+    // Update residual graph
+    while (current != source) {
+
+        int previous = parent[current];
+
+        // Forward edge
+        capacity[previous][current] -= newFlow;
+
+        // Reverse edge
+        capacity[current][previous] += newFlow;
+
+        current = previous;
+    }
         }
 
-        // Edmonds-Karp algorithm
-        int maxFlow(int source, int sink,
-                    vector<vector<int>>& capacity,
-                    vector<vector<int>>& adj,
-                    int n) {
-
-            int flow = 0;
-            vector<int> parent(n);
-
-            int newFlow;
-
-            while ((newFlow = bfs(source, sink,
-                capacity, adj, parent))) {
-
-                flow += newFlow;
-
-            int current = sink;
-
-            // Update residual graph
-            while (current != source) {
-
-                int previous = parent[current];
-
-                // Forward edge
-                capacity[previous][current] -= newFlow;
-
-                // Reverse edge
-                capacity[current][previous] += newFlow;
-
-                current = previous;
+        return flow;
             }
-                }
 
-                return flow;
-                    }
+            int main() {
 
-                    int main() {
+                int n = 6;
 
-                        int n = 6;
+                vector<vector<int>> capacity(n, vector<int>(n, 0));
+                vector<vector<int>> adj(n);
 
-                        vector<vector<int>> capacity(n, vector<int>(n, 0));
-                        vector<vector<int>> adj(n);
+                auto addEdge = [&](int u, int v, int cap) {
 
-                        auto addEdge = [&](int u, int v, int cap) {
+                    adj[u].push_back(v);
+                    adj[v].push_back(u); // needed for residual edge
 
-                            adj[u].push_back(v);
-                            adj[v].push_back(u); // needed for residual edge
+                    capacity[u][v] += cap;
+                };
 
-                            capacity[u][v] += cap;
-                        };
+                // Example graph
+                addEdge(0, 1, 16);
+                addEdge(0, 2, 13);
+                addEdge(1, 2, 10);
+                addEdge(2, 1, 4);
+                addEdge(1, 3, 12);
+                addEdge(3, 2, 9);
+                addEdge(2, 4, 14);
+                addEdge(4, 3, 7);
+                addEdge(3, 5, 20);
+                addEdge(4, 5, 4);
 
-                        // Example graph
-                        addEdge(0, 1, 16);
-                        addEdge(0, 2, 13);
-                        addEdge(1, 2, 10);
-                        addEdge(2, 1, 4);
-                        addEdge(1, 3, 12);
-                        addEdge(3, 2, 9);
-                        addEdge(2, 4, 14);
-                        addEdge(4, 3, 7);
-                        addEdge(3, 5, 20);
-                        addEdge(4, 5, 4);
+                int source = 0;
+                int sink = 5;
 
-                        int source = 0;
-                        int sink = 5;
+                cout << "Maximum Flow = "
+                << maxFlow(source, sink, capacity, adj, n)
+                << endl;
 
-                        cout << "Maximum Flow = "
-                        << maxFlow(source, sink, capacity, adj, n)
-                        << endl;
-
-                        return 0;
-                    }
+                return 0;
+            }
